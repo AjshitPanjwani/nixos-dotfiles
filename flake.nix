780e0def1d938d1db80aa60b4b150e4f2e48af15
akshit <akshitpanjwani2@gmail.com>
2026-07-23 14:48:23 +0530

    Add Catppuccin theme support in flake.nix

--- flake.nix
+++ flake.nix
@@ -3,17 +3,19 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    catppuccin.url = "github:catppuccin/nix";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, catppuccin, home-manager, ... }: {
     nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        catppuccin.nixosModules.catppuccin
         home-manager.nixosModules.home-manager
         {
           home-manager = {
@@ -21,6 +23,10 @@
             useUserPackages = true;
             backupFileExtension = "backup";
             
+            sharedModules = [
+              catppuccin.homeManagerModules.catppuccin 
+            ];
+
             users.alien = import ./home.nix;
           };
         }
